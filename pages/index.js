8c35c304f9ac987fe96f824a964101e8583459dd
milesrykerodazie <milesodazie@gmail.com>
2022-03-21 16:49:48 +0100

    edit

--- pages/index.js
+++ pages/index.js
@@ -11,22 +11,24 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       {/* Overall container */}
-      <div className="bg-bellefuBackground max-w-6xl mx-auto">
-        {/* second nav bar */}
-        <div>
-          {" "}
-          <HeaderSearch />
-        </div>
-        {/* main body */}
-        <div className="flex">
-          {/* cstegory side bar */}
-          <div className="w-72 min-h-screen bg-bellefuWhite">
-            category nav bar
+      <div className="bg-bellefuBackground">
+        <div className="max-w-6xl mx-auto">
+          {/* second nav bar */}
+          <div>
+            {" "}
+            <HeaderSearch />
           </div>
-          {/* list of products & slider */}
-          <div className="flex-1">
-            <div>the slider here</div>
-            <div>The products here</div>
+          {/* main body */}
+          <div className="flex">
+            {/* cstegory side bar */}
+            <div className="w-72 min-h-screen bg-bellefuWhite">
+              category nav bar
+            </div>
+            {/* list of products & slider */}
+            <div className="flex-1">
+              <div>the slider here</div>
+              <div>The products here</div>
+            </div>
           </div>
         </div>
       </div>
