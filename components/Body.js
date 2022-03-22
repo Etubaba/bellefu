35ca208e28d28fd0eeb32050d5e03f0817dc5097
Godknows Egi <godknowsegiboy00@gmail.com>
2022-03-22 13:45:16 +0100

    omo

--- components/Body.js
+++ components/Body.js
@@ -2,7 +2,7 @@ import React from "react";
 import ProductList from "./ProductList";
 import { productData } from "../productData";
 import Slider from "./slider/Slider";
-
+import Product from "./products/Product"
 const Body = () => {
   return (
     <div className=" overflow-y-scroll h-screen">
@@ -10,7 +10,9 @@ const Body = () => {
         <Slider />
       </div>
       {/* product title header section */}
-      <div></div>
+      <div>
+        <Product/>
+      </div>
       {/* product list */}
       <div className="bg-bellefuBackground mt-3 rounded-md grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3 ">
         {productData.map((product) => (
