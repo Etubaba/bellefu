ace5bcc616ea1004ce841e34f426025e726fcc72
Onyedikachukwu <isiwuemma@gmail.com>
2022-03-28 13:15:55 +0100

    register, login and update

--- components/usercomponent/RegisterHeader.js
+++ components/usercomponent/RegisterHeader.js
@@ -2,7 +2,7 @@ import NavLink from "./NavLink"
 
 const RegisterHeader = () => {
   return (
-    <div className="w-[55%] mx-auto mt-20 mb-6 flex rounded-lg border-2 px-9 py-6">
+    <div className="w-[55%] mx-auto mt-14 mb-6 flex rounded-lg border-2 px-9 py-4">
       <NavLink to="/register" className="mr-auto hover:cursor-pointer">Register</NavLink>
       <NavLink to="/login" className="hover:cursor-pointer">Login</NavLink>
     </div>
