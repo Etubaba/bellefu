13dbcc33025ed643caab21dd15068b6cc6c1fdff
Godknows Egi <godknowsegiboy00@gmail.com>
2022-03-30 16:30:39 +0100

    dhdh

--- components/mainPageComponents/slider/Slider.js
+++ components/mainPageComponents/slider/Slider.js
@@ -1,30 +1,13 @@
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
 import Image from "next/image";
+import {useState} from "react";
 
-export default function Slider() {
-  const slides = [
-    {
-      image:
-        "https://img.freepik.com/free-photo/organic-food-farm_342744-1362.jpg",
-      alternate: "belleful",
-    },
-    {
-      image:
-        "https://img.freepik.com/free-photo/close-up-box-with-ripe-vegetables_329181-4612.jpg",
-      alternate: "belleful",
-    },
-    {
-      image:
-        "https://img.freepik.com/free-photo/vegetables-basket-sunlight_268835-1168.jpg",
-      alternate: "belleful",
-    },
-    {
-      image:
-        "https://img.freepik.com/free-photo/corn-fields-agriculture-photo-theme-small-corn-plants_2379-276.jpg",
-      alternate: "belleful",
-    },
-  ];
+export default function Slider({slider}) {
+
+
+  const [newSlider]=slider;
+  
 
   return (
     <div>
@@ -34,12 +17,12 @@ export default function Slider() {
         autoPlay={true}
         infiniteLoop={true}
       >
-        {slides?.map((slideimage, index) => (
+        {newSlider.value?.map((slideimage, index) => (
           <img
             className="h-40 lg:h-80 w-full rounded-xl  "
             key={index}
-            src={slideimage.image}
-            alt={slideimage.alternate}
+            src={`https://bellefu.inmotionhub.xyz/get/sliders/image/${slideimage}`}
+            alt={slideimage}
           />
         ))}
       </Carousel>
