4ba2af05416012c326fa0c370be17836d2fd4df8
milesrykerodazie <milesodazie@gmail>
2022-03-31 11:47:45 +0100

    update

--- components/Dropdown.js
+++ components/Dropdown.js
@@ -15,7 +15,11 @@ const Dropdown = ({ category }) => {
           className="flex items-center mb-7 hover:bg-bellefuBackground p-1 rounded-md"
         >
           <div className="flex items-center flex-1 space-x-2 cursor-pointer select-none">
-            <img src={`https://bellefu.inmotionhub.xyz/get/category/image/${category.image}`} alt="icons" className="w-4 h-4" />
+            <img
+              src={`https://bellefu.inmotionhub.xyz/get/category/image/${category.image}`}
+              alt="icons"
+              className="w-4 h-4"
+            />
             <h5
               className="text-bellefuBlack1 font-medium text-normal whitespace-nowrap"
               onClick={() => router.push("/category/id")}
@@ -38,11 +42,11 @@ const Dropdown = ({ category }) => {
         <div
           className={
             open
-              ? "inline-block w-full -mt-6 space-y-3 text-sm text-bellefuBlack1 select-none bg-bellefuBackground"
+              ? "inline-block w-full -mt-6 space-y-3 text-sm text-bellefuBlack1 select-none bg-bellefuWhite"
               : "hidden"
           }
         >
-          <div className="border-t-2" />
+          <div className=" border-t-2" />
           {category.sub_category.map((child) => (
             <DropdownItems key={child.subCatId} child={child} />
           ))}
@@ -60,7 +64,8 @@ const Dropdown = ({ category }) => {
             <img
               src={`https://bellefu.inmotionhub.xyz/get/category/image/${category.image}`}
               alt="icons"
-              className="w-4 h-4" />
+              className="w-4 h-4"
+            />
             <h5
               className="text-bellefuBlack1 text-normal font-medium whitespace-nowrap"
               onClick={() => router.push("/category/id")}
---
+++ components/SingleProduct/SingleProductList.js
@@ -0,0 +1,45 @@
+import React from "react";
+import Image from "next/image";
+import { MdLocationOn } from "react-icons/md";
+import { BsHeart } from "react-icons/bs";
+import { MdOutlineMessage, MdCall } from "react-icons/md";
+
+const SingleProductList = ({ product }) => {
+  return (
+    <div className="bg-bellefuWhite p-3 rounded-b-md">
+      <img
+        src="/bellefu/bellefu1.jpg"
+        className="rounded-md w-full h-44 object-cover"
+      />
+      <p className="capitalize text-medium">{product.title.substring(0, 20)}</p>
+      <div className="flex items-center space-x-2">
+        <MdLocationOn className="w-4 h-4 text-bellefuBlack1" />
+        <div className="flex items-center space-x-1">
+          <p className="text-bellefuBlack1 text-sm capitalize">
+            {product.stateName},
+          </p>
+          <p className="text-bellefuBlack1 text-sm capitalize">
+            {product.countryName}
+          </p>
+        </div>
+      </div>
+      <div className="flex items justify-between">
+        <p className="text-bellefuGreen flex font-poppins font-semibold">
+          <p className="mr-1">$</p>
+          1234
+        </p>
+        <BsHeart className="w-4 h-4 text-bellefuOrange" />
+      </div>
+      <div className="flex items-center mt-2 space-x-3">
+        <button className="bg-bellefuOrange rounded-md w-full flex items-center justify-center py-4">
+          <MdOutlineMessage className="!text-white" />
+        </button>
+        <button className="bg-bellefuGreen  rounded-md w-full flex items-center justify-center py-4">
+          <MdCall className="text-white " />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SingleProductList;
