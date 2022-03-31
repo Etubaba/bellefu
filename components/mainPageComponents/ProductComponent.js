dce89d2e2f201f2f99ff65759e051e9220e467cc
milesrykerodazie <milesodazie@gmail>
2022-03-31 16:00:29 +0100

    update

--- components/mainPageComponents/ProductComponent.js
+++ components/mainPageComponents/ProductComponent.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { productData } from "../../productData";
 import MainProductHeader from "./MainProductHeader";
 import ProductList from "./ProductList";
-import { useRouter } from "next/router";
+
 import { countryChoice } from "../../features/bellefuSlice";
 import { useSelector } from "react-redux";
 import axios from "axios";
@@ -11,7 +11,6 @@ const ProductComponent = ({ products, currency }) => {
   const [countryData, setCountryData] = useState([]);
 
   const getCountry = useSelector((state) => state.bellefu.countrySelected);
-  const router = useRouter();
 
   useEffect(() => {
     const newProducts = async () => {
@@ -35,10 +34,7 @@ const ProductComponent = ({ products, currency }) => {
       <MainProductHeader />
       <div className="bg-bellefuBackground mt-1 rounded-md grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-1 grid-flow-row-dense ">
         {main.map((product) => (
-          <div
-            onClick={() => router.push(`/product/${product.productId}`)}
-            className="cursor-pointer"
-          >
+          <div className="cursor-pointer">
             <ProductList
               key={product.productId}
               currency={currency}
