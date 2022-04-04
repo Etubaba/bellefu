7066528a4d898cbc4631517f50df522157c749dc
milesrykerodazie <milesodazie@gmail.com>
2022-04-04 13:24:57 +0100

    update on responsive category page

--- pages/category/[id].js
+++ pages/category/[id].js
@@ -76,7 +76,7 @@ const Product = () => {
 
   console.log(state);
   return (
-    <div className="max-w-[90%] mx-auto">
+    <div className="max-w-[95%] lg:max-w-[90%] mx-auto">
       <HeaderSearch
         dialet={index?.defaultLanguage}
         state={index?.countryStates}
@@ -86,9 +86,9 @@ const Product = () => {
         location={index?.defaultCountry}
       />
 
-      <div className="flex">
+      <div className="flex flex-col lg:flex-row">
         {/* sidebar section */}
-        <div className="w-80 bg-bellefuWhite rounded-md mr-3 pb-5 px-3 pt-5 space-y-10 h-5/6">
+        <div className="hidden lg:inline w-80 bg-bellefuWhite rounded-md mr-3 pb-5 px-3 pt-5 space-y-10 h-5/6">
           {/* <h4 className="sticky top-0 bg-bellefuWhite p-5" /> */}
 
           {/* select category */}
@@ -370,6 +370,292 @@ const Product = () => {
             </button>
           </div>
         </div>
+
+        {/* mobile view sidebar section */}
+        <div className="lg:hidden mt-3">
+          <div className="flex space-x-3 items-center">
+            {/* category and sub category container */}
+            <div className="flex flex-col w-full">
+              {/* select category */}
+              <div>
+                <p className="text-bellefuBlack1 font-bold ">
+                  Select Category:
+                </p>
+                <div className="flex items-center mb-2 hover:bg-bellefuBackground p-3 rounded-md border mt-4 relative">
+                  <div className="flex items-center flex-1 space-x-3 cursor-pointer select-none">
+                    <img
+                      src="/bellefu-images/image1.svg"
+                      alt="icons"
+                      className="w-6 h-6"
+                    />
+                    <h5 className="text-bellefuBlack1 font-medium whitespace-nowrap">
+                      {/* Agricultural Produce */}
+                      {product[0]?.CatName}
+                    </h5>
+                  </div>
+                  {open === false ? (
+                    <div onClick={() => setOpen(!open)}>
+                      <AiOutlineCaretRight className="text-gray-300 cursor-pointer" />
+                    </div>
+                  ) : (
+                    <div onClick={() => setOpen(!open)}>
+                      <AiOutlineCaretDown className="text-gray-300 cursor-pointer" />
+                    </div>
+                  )}
+                </div>
+                {open === true ? (
+                  <div className="absolute w-72 bg-bellefuWhite rounded mx-auto border -mt-2 transition duration-300 ease-in z-40">
+                    <ul className="rounded px-5 py-4">
+                      {index?.categories.map((cat) => (
+                        <li
+                          key={cat.id}
+                          onClick={() => {
+                            setOpen(false);
+                            dispatch(updateIdpath(cat.id));
+                            setCatUpdate(cat.id);
+                          }}
+                          className="px-4 py-3 hover:bg-bellefuBackground flex space-x-5 items-center cursor-pointe rounded"
+                        >
+                          <img
+                            src={`https://bellefu.inmotionhub.xyz/get/category/image/${cat.image}`}
+                            alt="icons"
+                            className="w-6 h-6 object-cover"
+                          />
+                          <span>{cat.name}</span>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                ) : null}
+              </div>
+              {/* end of select category */}
+
+              {/* select sub category */}
+              <div>
+                <p className="text-bellefuBlack1 font-bold ">Sub Category:</p>
+                <div className="flex items-center mb-2 hover:bg-bellefuBackground p-3 rounded-md border mt-4 relative">
+                  <div className="flex items-center flex-1 space-x-3 cursor-pointer select-none">
+                    <h5 className="text-bellefuBlack1 font-medium whitespace-nowrap">
+                      {subCatText === null ? " Select Subcategory" : subCatText}
+                    </h5>
+                  </div>
+                  {open1 === false ? (
+                    <div onClick={() => setOpen1(!open1)}>
+                      <AiOutlineCaretRight className="text-gray-300 cursor-pointer" />
+                    </div>
+                  ) : (
+                    <div onClick={() => setOpen1(!open1)}>
+                      <AiOutlineCaretDown className="text-gray-300 cursor-pointer" />
+                    </div>
+                  )}
+                </div>
+                {open1 === true ? (
+                  <div className="absolute w-72 bg-bellefuWhite rounded mx-auto border -mt-2 transition duration-300 ease-in z-40">
+                    <ul className="rounded px-5 py-4">
+                      {subcat?.map((item) => (
+                        <li
+                          onClick={() => {
+                            setOpen1(!open1);
+                            setSubCatText(item.subCatName);
+                          }}
+                          key={item.id}
+                          className="px-4 py-3 hover:bg-bellefuBackground flex space-x-5 items-center cursor-pointe rounded"
+                        >
+                          <span>{item.subCatName}</span>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                ) : null}
+              </div>
+              {/* end of sub category */}
+            </div>
+            {/* end of category and sub category container */}
+            {/* container for state and popular products */}
+            <div className="flex flex-col w-full">
+              {/* select country */}
+              <div>
+                <p className="text-bellefuBlack1 font-bold ">Select State:</p>
+                <div className="flex items-center mb-2 hover:bg-bellefuBackground p-3 rounded-md border mt-4 relative">
+                  <div className="flex items-center flex-1 space-x-3 cursor-pointer select-none">
+                    <h5 className="text-bellefuBlack1 font-medium whitespace-nowrap">
+                      State
+                    </h5>
+                  </div>
+                  {open2 === false ? (
+                    <div onClick={() => setOpen2(!open2)}>
+                      <AiOutlineCaretRight className="text-gray-300 cursor-pointer" />
+                    </div>
+                  ) : (
+                    <div onClick={() => setOpen2(!open2)}>
+                      <AiOutlineCaretDown className="text-gray-300 cursor-pointer" />
+                    </div>
+                  )}
+                </div>
+                {open2 === true ? (
+                  <div className="absolute w-60 bg-bellefuWhite rounded mx-auto border -mt-2 transition duration-300 ease-in z-40">
+                    <ul className="rounded h-80 overflow-y-scroll px-5 py-4">
+                      {province.map((state) => (
+                        <li
+                          key={state.id}
+                          className="px-4 py-3 hover:bg-bellefuBackground flex space-x-5 items-center cursor-pointe rounded"
+                        >
+                          <span>{state.name}</span>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                ) : null}
+              </div>
+              {/* end of select country */}
+              {/* select products */}
+              <div>
+                <p className="text-bellefuBlack1 font-bold ">Sub Product:</p>
+                <div className="flex items-center mb-2 hover:bg-bellefuBackground px-2 py-3 rounded-md border mt-4 relative">
+                  <div className="flex items-center flex-1 space-x-3 cursor-pointer select-none">
+                    <h5 className="text-bellefuBlack1 font-medium whitespace-nowrap">
+                      Popular Products
+                    </h5>
+                  </div>
+                  {open3 === false ? (
+                    <div onClick={() => setOpen3(!open3)}>
+                      <AiOutlineCaretRight className="text-gray-400 cursor-pointer" />
+                    </div>
+                  ) : (
+                    <div onClick={() => setOpen3(!open3)}>
+                      <AiOutlineCaretDown className="text-bellefuOrange cursor-pointer" />
+                    </div>
+                  )}
+                </div>
+                <div className="absolute w-60 mx-auto bg-transparent -mt-4 z-40">
+                  {open3 === true ? (
+                    <div className="bg-bellefuWhite rounded border transition duration-300 ease-in z-40 shadow-md">
+                      <ul className="rounded px-2 py-3">
+                        <div className="flex items-center space-x-4 mb-2 bg-bellefuBackground px-2 rounded-md py-1">
+                          <img
+                            src="/bellefu-images/orange.svg"
+                            alt="icons"
+                            className="w-3 h-3 object-cover"
+                          />
+                          <p className="text-xs text-bellefuBlack1 font-normal whitespace-nowrap">
+                            Popular Products
+                          </p>
+                        </div>
+                        <li className=" py-1 hover:bg-bellefuBackground flex space-x-3 items-center cursor-pointer rounded">
+                          <img
+                            src="/bellefu-images/acending.svg"
+                            alt="icons"
+                            className="w-3 h-3 object-cover"
+                          />
+                          <span className="text-sm text-bellefuBlack1">
+                            Acending
+                          </span>
+                        </li>
+                        <li className=" py-1 hover:bg-bellefuBackground flex space-x-3 items-center cursor-pointer rounded">
+                          <img
+                            src="/bellefu-images/decending.svg"
+                            alt="icons"
+                            className="w-3 h-3 object-cover"
+                          />
+                          <span className="text-sm text-bellefuBlack1">
+                            Decending
+                          </span>
+                        </li>
+                        <li className=" py-2 hover:bg-bellefuBackground flex space-x-3 items-center cursor-pointer rounded">
+                          <img
+                            src="/bellefu-images/new-in.svg"
+                            alt="icons"
+                            className="w-3 h-3 object-cover"
+                          />
+                          <span className="text-sm text-bellefuBlack1">
+                            New In
+                          </span>
+                        </li>
+                        <li className=" py-2 hover:bg-bellefuBackground flex space-x-3 items-center cursor-pointer rounded">
+                          <img
+                            src="/bellefu-images/highestprice.svg"
+                            alt="icons"
+                            className="w-3 h-3 object-cover"
+                          />
+                          <span className="text-sm text-bellefuBlack1">
+                            Highest Price
+                          </span>
+                        </li>
+                        <li className=" py-2 hover:bg-bellefuBackground flex space-x-3 items-center cursor-pointer rounded">
+                          <img
+                            src="/bellefu-images/lowestprice.svg"
+                            alt="icons"
+                            className="w-3 h-3 object-cover"
+                          />
+                          <span className="text-sm text-bellefuBlack1">
+                            Lowest Price
+                          </span>
+                        </li>
+                      </ul>
+                    </div>
+                  ) : null}
+                </div>
+              </div>
+              {/* end of select products */}
+            </div>
+          </div>
+
+          {/* price section */}
+          <div className="border rounded px-2 py-3">
+            <div className="flex items-center px-2 rounded-md relative">
+              <div className="flex items-center flex-1 space-x-3 cursor-pointer select-none">
+                <h5 className="text-bellefuBlack1 font-medium whitespace-nowrap">
+                  Price
+                </h5>
+              </div>
+              {open4 === false ? (
+                <div onClick={() => setOpen4(!open4)}>
+                  <AiOutlineCaretRight className="text-gray-400 cursor-pointer" />
+                </div>
+              ) : (
+                <div onClick={() => setOpen4(!open4)}>
+                  <AiOutlineCaretDown className="text-bellefuOrange cursor-pointer" />
+                </div>
+              )}
+            </div>
+            <div className=" z-40">
+              {open4 === true ? (
+                <div
+                  className={
+                    open4 === true
+                      ? "w-full px-3 mt-4 bg-bellefuWhite rounded mx-auto transition duration-300 ease-in space-y-3"
+                      : ""
+                  }
+                >
+                  <Range />
+                  <div className="flex space-x-6 items-center justify-center">
+                    <div className="flex items-center space-x-2 mt-1">
+                      <p className="text-bellefuBlack1 text-normal whitespace-nowrap">
+                        From :
+                      </p>
+                      <input
+                        type="number"
+                        className="border outline-none px-2 py-1 rounded-sm w-full text-sm text-bellefuBlack1"
+                      />
+                    </div>
+                    <div className="flex items-center space-x-2">
+                      <p className="text-bellefuBlack1 text-normal whitespace-nowrap">
+                        To :
+                      </p>
+                      <input
+                        type="number"
+                        className="border outline-none px-2 py-1 rounded-sm w-full text-sm text-bellefuBlack1"
+                      />
+                    </div>
+                  </div>
+                </div>
+              ) : null}
+            </div>
+          </div>
+
+          {/* end of price section */}
+        </div>
+        {/* end of mobile view section */}
 
         {/* product session */}
         <div className="flex-1">
