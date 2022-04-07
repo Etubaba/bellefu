c67eb022e450ef26b8f0af0df8f96bb42dcd150c
milesrykerodazie <milesodazie@gmail.com>
2022-04-07 10:40:12 +0100

    similar product done

--- components/SingleProduct/SimilarProducts.js
+++ components/SingleProduct/SimilarProducts.js
@@ -1,13 +1,16 @@
 import React from "react";
 import SingleProductList from "./SingleProductList";
 
-const SimilarProducts = ({ products }) => {
-  console.log("products =>", products);
+const SimilarProducts = ({ similarProductDetails }) => {
+  const slicedSPDetails = similarProductDetails.slice(0, 4);
   return (
     <div className="px-4 bg-bellefuWhite rounded-b-md">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-1 grid-flow-row-dense">
-        {products.map((product) => (
-          <SingleProductList key={product.id} product={product} />
+        {slicedSPDetails.map((slicedSPDetail) => (
+          <SingleProductList
+            key={slicedSPDetail.productId}
+            similarProductDetail={slicedSPDetail}
+          />
         ))}
       </div>
     </div>
--- components/SingleProduct/SingleProductList.js
+++ components/SingleProduct/SingleProductList.js
@@ -4,29 +4,31 @@ import { MdLocationOn } from "react-icons/md";
 import { BsHeart } from "react-icons/bs";
 import { MdOutlineMessage, MdCall } from "react-icons/md";
 
-const SingleProductList = ({ product }) => {
+const SingleProductList = ({ similarProductDetail }) => {
   return (
     <div className="bg-bellefuWhite p-3 rounded-b-md">
       <img
         src="/bellefu/bellefu1.jpg"
         className="rounded-md w-full h-44 object-cover"
       />
-      <p className="capitalize text-medium">{product.title.substring(0, 20)}</p>
+      <p className="capitalize text-medium">
+        {similarProductDetail.title.substring(0, 20)}
+      </p>
       <div className="flex items-center space-x-2">
         <MdLocationOn className="w-4 h-4 text-bellefuBlack1" />
         <div className="flex items-center space-x-1">
           <p className="text-bellefuBlack1 text-sm capitalize">
-            {product.stateName},
+            {similarProductDetail.stateName},
           </p>
           <p className="text-bellefuBlack1 text-sm capitalize">
-            {product.countryName}
+            {similarProductDetail.countryName}
           </p>
         </div>
       </div>
       <div className="flex items justify-between">
         <p className="text-bellefuGreen flex font-poppins font-semibold">
           <p className="mr-1">$</p>
-          1234
+          {similarProductDetail.price}
         </p>
         <BsHeart className="w-4 h-4 text-bellefuOrange" />
       </div>
