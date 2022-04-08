a9348f22cefe0727c43c63870595cb53ca299c16
milesrykerodazie <milesodazie@gmail.com>
2022-04-08 11:23:28 +0100

    updates

--- components/Layout.js
+++ components/Layout.js
@@ -13,21 +13,24 @@ const Layout = ({ children }) => {
       {userId ? (
         <>
           {/* small header for popping mobile sidebar for user profile  */}
-          {/* <div className="lg:hidden flex items-center mt-1 bg-bellefuGreen justify-between rounded-md px-2 py-1">
+          <div className="lg:hidden flex items-center mt-1 bg-bellefuGreen justify-between rounded-md px-2 py-1">
             <p className="text-white font-semibold">Dashboard </p>
             <button className="" onClick={() => setOpen(!open)}>
               <FiMenu className="w-6 h-6 text-white" />
             </button>
-          </div> */}
+          </div>
           {/* end of small header for popping mobile sidebar for user profile  */}
           <div className="flex flex-col lg:flex-row">
             <div className="hidden lg:inline">
               <UserSideBar />
             </div>
-            <div className="lg:hidden">
-              {open && <MobileUserSidebar open={open} setOpen={setOpen} />}
-            </div>
-            <main className="flex-grow">{children}</main>
+            {open && (
+              <div className="lg:hidden">
+                {" "}
+                <MobileUserSidebar open={open} setOpen={setOpen} />
+              </div>
+            )}
+            <main className="flex-grow">{children}</main>{" "}
           </div>
         </>
       ) : (
