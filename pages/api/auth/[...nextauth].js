7b688607342f79016b0450914db17a251c538d35
Onyedikachukwu <isiwuemma@gmail.com>
2022-04-13 18:11:23 +0100

    update

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,21 +1,29 @@
 import NextAuth from "next-auth";
-//import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
-    // GithubProvider({
-    //   clientId: process.env.GITHUB_ID,
-    //   clientSecret: process.env.GITHUB_SECRET,
-    // }),
-    // ...add more providers here
     GoogleProvider({
         clientId: process.env.ID,
         clientSecret: process.env.SECRET,
-        profile(profile, tokens) {
-          return { ...profile, tokens }
-        }
     }),
   ],
+  callbacks: {
+    async jwt({token, account, profile}) {
+
+      if (profile?.email_verified) {
+        token.providerId = profile.sub
+        token.providerName = account.provider;
+      }
+
+      return token;
+    },
+    async session({session, token}) {
+      session.providerName = token.providerName;
+      session.providerId = token.providerId;
+
+      return session;
+    }
+  }
 })
