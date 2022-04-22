8b2ffc847d4a637fd2ff2275bb9be5bf69aa0984
Etumnu Prosper <johnikechukwu94@gmail.com>
2022-04-22 11:04:30 +0100

    done

--- components/categoryIdProducts/CategoryProductList.js
+++ components/categoryIdProducts/CategoryProductList.js
@@ -1,11 +1,69 @@
 import React, { useState } from "react";
-import { MdLocationOn } from "react-icons/md";
-import { BsHeart } from "react-icons/bs";
+import { MdLocationOn } from "react-icons/md"
 import { MdOutlineMessage, MdCall } from "react-icons/md";
+import { BsHeart, BsSuitHeartFill } from "react-icons/bs";
 import { useRouter } from "next/router";
+import axios from "axios";
+import { apiData } from "../../constant";
+import { toast } from "react-toastify";
+import { useSelector } from "react-redux";
+import { login } from "../../features/bellefuSlice";
 
 const CategoryProductList = ({ product }) => {
+  const [fav2, setFav2] = useState(false);
+
+  const favArr = useSelector((state) => state.bellefu?.favArr);
+  const userId = useSelector((state) => state.bellefu?.profileDetails?.id);
+  const isLoggedIn = useSelector(login);
   const router = useRouter();
+
+
+
+  const addFav = (e) => {
+    e.stopPropagation();
+    if (isLoggedIn) {
+      axios
+        .post(`${apiData}add/favorite`, {
+          userId: userId,
+          productId: product.productId,
+        })
+        .then((res) => {
+          console.log(res.data);
+          if (res.data.status) {
+            setFav2(!fav2);
+            toast.success(
+              `${product.title.substring(0, 20)} added to favourite`
+            );
+          }
+        });
+    }
+  };
+
+
+  const removeFav = () => {
+    axios
+      .post(`${apiData}delete/favorite/webindex`, {
+        productId: product.productId,
+        userId: userId,
+      })
+      .then((res) => {
+        if (res.data.status) {
+          setFav2(!fav2);
+          // const cleanArr = fav.filter(
+          //   (items) => items !== product.productId
+          // );
+          // setClean(cleanArr);
+          toast.error(
+            `${product.title.substring(0, 20)} removed from favorite product`,
+            {
+              position: "top-right",
+            }
+          );
+        }
+      });
+  };
+
+
 
   // console.log(product)
   return (
@@ -34,7 +92,15 @@ const CategoryProductList = ({ product }) => {
         <p className="text-bellefuGreen font-poppins font-semibold">
           ₦ {product?.price}
         </p>
-        <BsHeart className="w-5 h-5 text-bellefuOrange" />
+        {fav2 || favArr?.includes(product.productId) ? (
+          <div onClick={removeFav} className="cursor-pointer">
+            <BsSuitHeartFill className="w-4 h-4 text-bellefuOrange" />
+          </div>
+        ) : (
+          <div onClick={addFav} className="cursor-pointer">
+            <BsHeart className="w-4 h-4 text-bellefuOrange" />
+          </div>
+        )}
       </div>
       <div className="flex items-center space-x-3 mt-2">
         <button className="bg-bellefuOrange w-full  rounded-md py-3 flex items-center justify-center">
