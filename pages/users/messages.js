f09330c6de0a2dbde4947fc7e9f8a27499412441
Etumnu Prosper <johnikechukwu94@gmail.com>
2022-04-25 17:12:26 +0100

    msg update

--- pages/users/messages.js
+++ pages/users/messages.js
@@ -90,7 +90,7 @@ const messages = ({ data1 }) => {
     };
 
     getChat();
-  }, [message, receiverId]);
+  }, [message, receiverId, sent]);
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -289,19 +289,38 @@ const messages = ({ data1 }) => {
                         : "flex justify-end"
                     }
                   >
-                    <div
-                      className={
-                        item.from_id !== senderId
-                          ? "after:content-[''] after:absolute after:right-[100%] after:top-[0] after:border-l-gray-100  relative max-w-xl mb-4 px-4 py-2 md:px-8 md:py-4 text-gray-700 bg-gray-100 rounded shadow"
-                          : "relative max-w-xl mb-4 px-4 py-2 md:px-8 md:py-4 text-gray-100 bg-bellefuGreen rounded shadow"
-                      }
-                    >
-                      <div className="block">
-                        <span className="block text-sm md:text-lg">
-                          {item.body}
-                        </span>
+
+
+                    {item.attachment === null ?
+                      <div
+                        className={
+                          item.from_id !== senderId
+                            ? "after:content-[''] after:absolute after:right-[100%] after:top-[0] after:border-l-gray-100  relative max-w-xl mb-4 px-4 py-2 md:px-8 md:py-4 text-gray-700 bg-gray-100 rounded shadow"
+                            : "relative max-w-xl mb-4 px-4 py-2 md:px-8 md:py-4 text-gray-100 bg-bellefuGreen rounded shadow"
+                        }
+                      >
+                        <div className="block">
+                          <span className="block text-sm md:text-lg">
+                            {item.body}
+                          </span>
+                        </div>
+                      </div> :
+
+                      <div
+                        className={
+                          item.from_id !== senderId
+                            ? "after:content-[''] after:absolute after:right-[100%] after:top-[0] after:border-l-gray-100  relative max-w-xl mb-4 px-4 py-2 md:px-8 md:py-4 text-gray-700 bg-gray-100 rounded shadow"
+                            : "relative max-w-xl mb-4 px-4 py-2 md:px-8 md:py-4 text-gray-100 bg-bellefuGreen rounded shadow"
+                        }>
+                        <img
+                          src={`https://bellefu.inmotionhub.xyz/get/chat/image/${item.attachment}`}
+                          alt='error'
+                          className=' md:w-28 md:h-28 w-20 h-20 object-cover rounded-md'
+                        />
+
                       </div>
-                    </div>
+
+                    }
                   </li>
                   <span
                     className={
