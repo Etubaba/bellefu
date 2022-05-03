ac52a918b406018b824152d547ccbe03587a4b47
milesrykerodazie <milesodazie@gmail.com>
2022-05-03 12:14:39 +0000

    update

--- components/MobileCategorybar/MobileCategoryItem.js
+++ components/MobileCategorybar/MobileCategoryItem.js
@@ -1,6 +1,20 @@
-const MobileCategoryItem = ({ child }) => {
+import { useDispatch } from "react-redux";
+import { Subcat } from "../../features/bellefuSlice";
+import { useRouter } from "next/router";
+
+const MobileCategoryItem = ({ child, setOpen }) => {
+  const router = useRouter();
+  const dispatch = useDispatch();
   return (
-    <div className="">
+    <div
+      className=""
+      onClick={() => {
+        {
+          dispatch(Subcat(child.subCatId)),
+            router.push("/category/${child.subCatId}");
+        }
+      }}
+    >
       <p className="hover:bg-bellefuOrange px-1 py-2 rounded-br-md rounded-tr-md">
         {child?.subCatName}
       </p>
