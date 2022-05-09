1e801eae5442b16e6c6515d1a404ad0750b84187
Onyedikachukwu <isiwuemma@gmail.com>
2022-05-09 11:17:44 +0100

    update

--- components/SingleProduct/SingleProductDescription.js
+++ components/SingleProduct/SingleProductDescription.js
@@ -267,7 +267,7 @@ const SingleProductDescription = ({ productDetails }) => {
     <Head>
       <title>{productDetails[0]?.title}</title>
       <meta name="description" content={description} />
-      <meta name="og:title" content={`Product Name: ${productDetails[0]?.title}`} />
+      <meta name="og:title" content={productDetails[0]?.title} />
       <meta name="og:description" content={description} />
       <meta name="og:image" content={`https://bellefu.inmotionhub.xyz/get/product/image/${productDetails[0]?.images[0]}`} />
     </Head>
