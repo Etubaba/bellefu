fce9357365d02cfebcb716e0e6e3af8932fe2c5a
Onyedikachukwu <isiwuemma@gmail.com>
2022-05-11 13:46:35 +0100

    update

--- pages/shared.js
+++ pages/shared.js
@@ -22,8 +22,8 @@ const SharedProduct = () => {
   return (
     <Head>
       <meta name="og:image" content={singleProduct?.image} />
-      <meta name="og:title" content={singleProduct?.title} />
-      <meta name="og:description" content={singleProduct?.description} />
+      <meta name="og:title" content={singleProduct?.title || "my title"} />
+      <meta name="og:description" content={singleProduct?.description || "my description"} />
       <meta name="og:url" content={singleProduct?.url} />
     </Head>
   )
