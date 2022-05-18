cdae7c6d4037367083da1665af902e29f1efa913
Godknows Egi <godknowsegiboy00@gmail.com>
2022-05-18 12:20:38 +0100

    update after chidera spoil API

--- components/postAdsComponent/State.js
+++ components/postAdsComponent/State.js
@@ -1,55 +1,61 @@
-import * as React from 'react';
-import SelectUnstyled, { selectUnstyledClasses } from '@mui/base/SelectUnstyled';
-import OptionUnstyled, { optionUnstyledClasses } from '@mui/base/OptionUnstyled';
-import PopperUnstyled from '@mui/base/PopperUnstyled';
-import {handleStateUpdate} from "../../features/bellefuSlice";
+import * as React from "react";
+import SelectUnstyled, {
+  selectUnstyledClasses,
+} from "@mui/base/SelectUnstyled";
+import OptionUnstyled, {
+  optionUnstyledClasses,
+} from "@mui/base/OptionUnstyled";
+import PopperUnstyled from "@mui/base/PopperUnstyled";
+import { handleStateUpdate } from "../../features/bellefuSlice";
 import { useDispatch } from "react-redux";
-import { styled } from '@mui/system';
+import { styled } from "@mui/system";
+import axios from "axios";
+
 
 const blue = {
-  100: '#DAECFF',
-  200: '#99CCF3',
-  400: '#3399FF',
-  500: '#007FFF',
-  600: '#0072E5',
+  100: "#DAECFF",
+  200: "#99CCF3",
+  400: "#3399FF",
+  500: "#007FFF",
+  600: "#0072E5",
   900: "E8F0FE",
 };
 
 const grey = {
-  100: '#E7EBF0',
-  200: '#E0E3E7',
-  300: '#CDD2D7',
-  400: '#B2BAC2',
-  500: '#A0AAB4',
-  600: '#6F7E8C',
-  700: '#3E5060',
-  800: '#2D3843',
-  900: '#1A2027',
+  100: "#E7EBF0",
+  200: "#E0E3E7",
+  300: "#CDD2D7",
+  400: "#B2BAC2",
+  500: "#A0AAB4",
+  600: "#6F7E8C",
+  700: "#3E5060",
+  800: "#2D3843",
+  900: "#1A2027",
 };
 
-const StyledButton = styled('button')(
+const StyledButton = styled("button")(
   ({ theme }) => `
   font-family: IBM Plex Sans, sans-serif;
   font-size: 0.875rem;
   box-sizing: border-box;
   min-height: calc(1.5em + 17px);
   min-width: 100%;
-  background: ${theme.palette.mode === 'dark' ? grey[900] : "#ffffff"};
-  border: 1px solid ${theme.palette.mode === 'dark' ? grey[800] : grey[300]};
+  background: ${theme.palette.mode === "dark" ? grey[900] : "#ffffff"};
+  border: 1px solid ${theme.palette.mode === "dark" ? grey[800] : grey[300]};
   border-radius: 0.50em;
   margin-top: 0.29em;
   padding: 5px;
   text-align: left;
   line-height: 1.5;
-  color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
+  color: ${theme.palette.mode === "dark" ? grey[300] : grey[900]};
 
   &:hover {
-    background: ${theme.palette.mode === 'dark' ? '' : grey[100]};
-    border-color: ${theme.palette.mode === 'dark' ? grey[700] : grey[400]};
+    background: ${theme.palette.mode === "dark" ? "" : grey[100]};
+    border-color: ${theme.palette.mode === "dark" ? grey[700] : grey[400]};
   }
 
   &.${selectUnstyledClasses.focusVisible} {
-    outline: 3px solid ${theme.palette.mode === 'dark' ? blue[900] : blue[900]};
+    outline: 3px solid ${theme.palette.mode === "dark" ? blue[900] : blue[900]};
   }
 
   &.${selectUnstyledClasses.expanded} {
@@ -62,10 +68,10 @@ const StyledButton = styled('button')(
     content: '▾';
     float: right;
   }
-  `,
+  `
 );
 
-const StyledListbox = styled('ul')(
+const StyledListbox = styled("ul")(
   ({ theme }) => `
   font-family: IBM Plex Sans, sans-serif;
   font-size: 0.875rem;
@@ -73,15 +79,15 @@ const StyledListbox = styled('ul')(
   padding: 4px;
   margin: 10px 0;
   min-width: 26vw;
-  background: ${theme.palette.mode === 'dark' ? grey[900] : '#fff'};
-  border: 1px solid ${theme.palette.mode === 'dark' ? grey[800] : grey[300]};
+  background: ${theme.palette.mode === "dark" ? grey[900] : "#fff"};
+  border: 1px solid ${theme.palette.mode === "dark" ? grey[800] : grey[300]};
   border-radius: 0.50em;
-  color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
+  color: ${theme.palette.mode === "dark" ? grey[300] : grey[900]};
   overflow: auto;
   outline: 0px;
   overflow-y:scroll;
   height:36vh
-  `,
+  `
 );
 
 const StyledOption = styled(OptionUnstyled)(
@@ -96,29 +102,29 @@ const StyledOption = styled(OptionUnstyled)(
   }
 
   &.${optionUnstyledClasses.selected} {
-    background-color: ${theme.palette.mode === 'dark' ? blue[900] : blue[900]};
-    color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
+    background-color: ${theme.palette.mode === "dark" ? blue[900] : blue[900]};
+    color: ${theme.palette.mode === "dark" ? grey[300] : grey[900]};
   }
 
   &.${optionUnstyledClasses.highlighted} {
-    background-color: ${theme.palette.mode === 'dark' ? grey[800] : grey[100]};
-    color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
+    background-color: ${theme.palette.mode === "dark" ? grey[800] : grey[100]};
+    color: ${theme.palette.mode === "dark" ? grey[300] : grey[900]};
   }
 
   &.${optionUnstyledClasses.highlighted}.${optionUnstyledClasses.selected} {
-    background-color: ${theme.palette.mode === 'dark' ? blue[900] : blue[900]};
-    color: ${theme.palette.mode === 'dark' ? blue[900] : blue[900]};
+    background-color: ${theme.palette.mode === "dark" ? blue[900] : blue[900]};
+    color: ${theme.palette.mode === "dark" ? blue[900] : blue[900]};
   }
 
   &.${optionUnstyledClasses.disabled} {
-    color: ${theme.palette.mode === 'dark' ? grey[700] : grey[400]};
+    color: ${theme.palette.mode === "dark" ? grey[700] : grey[400]};
   }
 
   &:hover:not(.${optionUnstyledClasses.disabled}) {
-    background-color: ${theme.palette.mode === 'dark' ? grey[800] : grey[100]};
-    color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
+    background-color: ${theme.palette.mode === "dark" ? grey[800] : grey[100]};
+    color: ${theme.palette.mode === "dark" ? grey[300] : grey[900]};
   }
-  `,
+  `
 );
 
 const StyledPopper = styled(PopperUnstyled)`
@@ -136,33 +142,40 @@ const CustomSelect = React.forwardRef(function CustomSelect(props, ref) {
   return <SelectUnstyled {...props} ref={ref} components={components} />;
 });
 
-export default function UnstyledSelectSimple4({countryStuffs,states,checker2,catchLgas}) {
+export default function UnstyledSelectSimple4({
+  states,
+  checker2,
+  catchLgas,
+}) {
+  const dispatch = useDispatch();
 
-  const dispatch =useDispatch();
+  const handleThings = (counts) => {
+    axios
+      .get(
+        `https://bellefu.inmotionhub.xyz/api/web30/get/postadd/lgas/${counts.code}`
+      )
+      .then((response) => {
 
-  const handleThings=(counts)=>{
-    // setCat(counts.name);
-    const newLgaArr = countryStuffs.lga.filter((lgas) => lgas.stateCode === counts.code);
-   dispatch(handleStateUpdate(counts.code))
-    // console.log(counts);
-    // console.log(countryStuffs.lga);
-    // console.log(newLgaArr);
-    catchLgas(newLgaArr,counts.name);
-  }
-
+        const newLgaArr = response?.data.lga;
+        dispatch(handleStateUpdate(counts.code));
+        // console.log(countryStuffs.lga);
+        // console.log(newLgaArr);
+        catchLgas(newLgaArr, counts.name);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   return (
-    <CustomSelect  disabled={checker2===""?true:false}>
-       {states?.map((counts,index)=>(
-       <span  onClick={() => handleThings(counts)}>
-       <StyledOption
-         key={index}
-         value={counts.name}
-       >
-         {counts.name}
-       </StyledOption>
-     </span>
-     ))}
+    <CustomSelect disabled={checker2 === "" ? true : false}>
+      {states?.map((counts, index) => (
+        <span onClick={() => handleThings(counts)}>
+          <StyledOption key={index} value={counts.name}>
+            {counts.name}
+          </StyledOption>
+        </span>
+      ))}
     </CustomSelect>
   );
-};
+}
