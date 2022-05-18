fc0ec00fd74e28629d3fe8c46f9f7e902839c0b1
Onyedikachukwu <isiwuemma@gmail.com>
2022-05-18 16:28:25 +0100

    update

--- components/CreateProductHeader.js
+++ components/CreateProductHeader.js
@@ -8,12 +8,12 @@ const CreateProductHeader = () => {
   if (clearHeader) return null;
 
   return (
-    <div className="flex items-center justify-between p-6">
-      <div>
-        <p className="text-2xl uppercase">notice:</p>
-        <p className="text-lg">You're required to complete your profile before you can post products. <Link href="#"><a>Click here to continue</a></Link> or ignore if you have already.</p>
+    <div className="flex items-center justify-between p-4">
+      <div className="flex space-x-4">
+        <p className="text-sm uppercase text-bellefuOrange">notice:</p>
+        <p className="text-sm">You're required to complete your profile before you can post products. <Link href="#"><a className="text-bellefuOrange hover:underline">Click here to continue</a></Link> or ignore if you have already.</p>
       </div>
-      <div className="text-xl" onClick={() => {setClearHeader(true)}}>&times;</div>
+      <div className="text-2xl hover:cursor-pointer" onClick={() => {setClearHeader(true)}}>&times;</div>
     </div>
   )
 }
