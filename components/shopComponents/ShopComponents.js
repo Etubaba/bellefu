ec80bcb9ce6832fb9cfc8a49723a907ca7d1e2a4
Etumnu Prosper <johnikechukwu94@gmail.com>
2022-05-19 17:11:34 +0100

    done again

--- components/shopComponents/ShopComponents.js
+++ components/shopComponents/ShopComponents.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { useRouter } from 'next/router'
+import { BsShopWindow } from "react-icons/bs";
+
 import ShopComponent from "./ShopComponent";
 
 const ShopComponents = ({ products }) => {
+
   const router = useRouter();
   const product = products.data
   return (
@@ -18,20 +21,26 @@ const ShopComponents = ({ products }) => {
         <div className="h-full px-2 lg:px-0 ">
           <div className="border mx-auto mt-2 lg:my-5 rounded-xl w-full lg:w-7/12 h-11/12 ">
             <div className="flex flex-col justify-center mt-24 mb-24 items-center">
-              <GiShoppingCart className="text-7xl lg:text-9xl mb-5 text-gray-600" />
+              <BsShopWindow className="text-7xl lg:text-9xl mb-5 text-gray-600" />
               <p className="text-sm capitalize lg:text-lg text-gray-600 px-2 text-center">
                 There is no product in this shop yet
               </p>
 
               <button
                 onClick={() => router.back()}
-                className="py-1 lg:py-3 hover:bg-pink-600 mt-16 px-8 lg:px-12 rounded-full bg-crystamolButton text-white text-sm lg:text-lg">
+                className="py-1 lg:py-3 hover:bg-orange-400 mt-16 px-8 lg:px-12 rounded-full bg-bellefuOrange text-white text-sm lg:text-lg">
                 Go back
               </button>
             </div>
           </div>
         </div>
       }
+
+
+
+
+
+
     </div>
   );
 };
--- pages/shop/[slug].js
+++ pages/shop/[slug].js
@@ -8,22 +8,78 @@ import { userDId } from "../../features/bellefuSlice";
 import { profileDetails } from "../../features/bellefuSlice";
 
 const Shop = ({ shopProducts }) => {
+  const [page, setPage] = React.useState(1);
 
   const shopDetails = shopProducts.data[0];
+  const ifProduct = shopProducts.data.length
 
+  const totalPage = shopProducts.last_page
+
+
+  const pageNumber = []
+  for (let i = 1; i <= totalPage; i++) {
+    pageNumber.push(i);
+  }
 
   return (
     <>
-
-      {/* <Head>
-        <title>{products[0].title}</title>
-      </Head> */}
       <div className="flex max-w-[95%] lg:max-w-[90%] mx-auto mt-28">
-        <ShopSideBar userDetails={shopDetails} />
+        {ifProduct > 0 && <ShopSideBar userDetails={shopDetails} />}
         <div className="flex-1">
           <ShopComponents products={shopProducts} />
-          <MobileShopSideBar userDetails={shopDetails} />
+          {ifProduct > 0 && <MobileShopSideBar userDetails={shopDetails} />}
         </div>
+
+
+        {ifProduct !== 0 && totalPage > 1 && (
+          <div className="flex justify-center md:mb-0 mb-8 md:mt-10 mt-7 items-center w-full ">
+            <button
+              onClick={() => {
+                if (page > 1) {
+                  setPage((prev) => prev - 1);
+                }
+              }}
+              className="flex bg-bellefuOrange hover:bg-orange-500 text-white px-1 md:px-4 md:py-2 rounded-md md:rounded-lg md:space-x-2 space-x-1 py-1"
+            >
+              <MdOutlineArrowBackIosNew className="mt-1" /> <span> Prev</span>
+            </button>
+
+            <span className="justify-center items-center mx-2 md:mx-4 px-2 md:px-4 flex space-x-2 md:space-x-6">
+              {pageNumber?.map((item, index) => (
+                <p
+                  onClick={() => setPage(item)}
+                  className={
+                    page === item
+                      ? "bg-bellefuGreen p-1 px-2 rounded-full text-white"
+                      : "cursor-pointer"
+                  }
+                  key={index}
+                >
+                  {item}
+                </p>
+              ))}
+            </span>
+
+            {ifProduct === 32 && (
+              <button
+                onClick={() => {
+                  if (page < totalPage) {
+                    {
+                      setPage((prev) => prev + 1);
+                    }
+                  }
+                }}
+                className="flex bg-bellefuGreen hover:bg-green-400 text-white px-1 md:px-4 md:py-2 rounded-md md:rounded-lg md:space-x-2 space-x-1 py-1"
+              >
+                <span> Next</span> <MdOutlineArrowForwardIos className="mt-1" />
+              </button>
+            )}
+          </div>
+        )}
+
+
+
+
       </div>
     </>
   );
