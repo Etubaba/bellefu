0cefaef7415be52eae69e0edebc4820e21b746ed
milesrykerodazie <milesodazie@gmail.com>
2022-05-19 13:23:24 +0000

    update

---
+++ pages/shopproduct/product.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+const ShopProduct = () => {
+  const [productDetails, setProductDetails] = useState([]);
+
+  // handling getting the product details of a user
+  useEffect(() => {
+    const getProductDetails = async () => {
+      const resProductDetails = await fetch(
+        `https://bellefu.inmotionhub.xyz/api/shop/view/single/${slug}/${productSlug}`
+      );
+      const details = await resProductDetails.json();
+      setProductDetails(await details?.data);
+    };
+    getProductDetails();
+  }, []);
+
+  console.log("details", details);
+  return (
+    <div>
+      <h1>hello shop product</h1>
+    </div>
+  );
+};
+
+export default ShopProduct;
+
+// //server side fetching of the full product details
+// export async function getServerSideProps(context) {
+//   const { slug, productSlug } = context.query;
+
+//   const requests = await fetch(
+//     `https://bellefu.inmotionhub.xyz/api/shop/view/single/${slug}/${productSlug}`
+//   ).then((res) => res.json());
+
+//   return {
+//     props: {
+//       details: requests,
+//     },
+//   };
+// }
