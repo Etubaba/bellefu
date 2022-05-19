5833b84a3fbd3ca33f83a368ec80741fae8c4102
Etumnu Prosper <johnikechukwu94@gmail.com>
2022-05-19 15:31:14 +0100

    pushing slice

--- pages/shopproduct/product.js
+++ pages/shopproduct/product.js
@@ -1,25 +1,31 @@
+import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 
 const ShopProduct = () => {
-  const pDetails = useSelector((state) => state.bellefu?.favArr);
+  const pDetails = useSelector((state) => state.bellefu?.shopProduct);
 
   const [productDetails, setProductDetails] = useState();
 
   // handling getting the product details of a user
   useEffect(() => {
     const getProductDetails = async () => {
-      const resProductDetails = await fetch(
-        `https://bellefu.inmotionhub.xyz/api/shop/view/single/${slug}/${productSlug}`
-      );
-      const details = await resProductDetails.json();
-      setProductDetails(await details?.data);
+      await axios.get(
+        `https://bellefu.inmotionhub.xyz/api/shop/view/single/${pDetails}`
+      ).then((res) => setProductDetails(res.data.data));
+
+      // const details = await resProductDetails.json();
+      // (await details?.data);
     };
     getProductDetails();
   }, []);
 
+
+  console.log('redux url', pDetails);
+
+  console.log('product', productDetails);
   return (
-    <div>
+    <div className='mt-28'>
       <h1>hello shop product</h1>
     </div>
   );
